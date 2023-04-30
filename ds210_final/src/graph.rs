2af460492ff944c1d841eb8d930b5f5b1c8916c2
jmijares0 <131937550+jmijares0@users.noreply.github.com>
2023-04-30 17:53:30 -0400

    graph.rs test
    added test functions to the graph.rs file

--- ds210_final/src/graph.rs
+++ ds210_final/src/graph.rs
@@ -37,3 +37,57 @@ impl Graph {
         self.edges.keys().cloned().collect()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_edge() {
+        let mut graph = Graph::new();
+        graph.add_edge(0, 1);
+        graph.add_edge(1, 2);
+        graph.add_edge(2, 3);
+
+        assert_eq!(graph.edges.get(&0), Some(&vec![1]));
+        assert_eq!(graph.edges.get(&1), Some(&vec![0, 2]));
+        assert_eq!(graph.edges.get(&2), Some(&vec![1, 3]));
+        assert_eq!(graph.edges.get(&3), Some(&vec![2]));
+    }
+
+    #[test]
+    fn test_degree() {
+        let mut graph = Graph::new();
+        graph.add_edge(0, 1);
+        graph.add_edge(1, 2);
+        graph.add_edge(2, 3);
+
+        assert_eq!(graph.degree(0), 1);
+        assert_eq!(graph.degree(1), 2);
+        assert_eq!(graph.degree(2), 2);
+        assert_eq!(graph.degree(3), 1);
+    }
+
+    #[test]
+    fn test_neighbors_at_distance_2() {
+        let mut graph = Graph::new();
+        graph.add_edge(0, 1);
+        graph.add_edge(1, 2);
+        graph.add_edge(1, 3);
+
+        let neighbors = graph.neighbors_at_distance_2(0);
+        let expected_neighbors: HashSet<usize> = [2, 3].iter().cloned().collect();
+        assert_eq!(neighbors, expected_neighbors);
+    }
+
+    #[test]
+    fn test_nodes() {
+        let mut graph = Graph::new();
+        graph.add_edge(0, 1);
+        graph.add_edge(1, 2);
+
+        let nodes = graph.nodes();
+        let expected_nodes: HashSet<usize> = [0, 1, 2].iter().cloned().collect();
+        assert_eq!(nodes.iter().cloned().collect::<HashSet<usize>>(), expected_nodes);
+    }
+}
